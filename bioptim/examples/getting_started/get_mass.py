9a777477a6dc61d0fc5f25e43f9716e8d0c7c8b1
Emma <emma.menon35@orange.fr>
2025-08-13 15:59:40 -0400

    get the mass of each segment of a .bioMod file

---
+++ bioptim/examples/getting_started/get_mass.py
@@ -0,0 +1,58 @@
+import re
+import pandas as pd 
+
+
+def extraire_masses(fichier_path):
+    """
+    Extract the mass values of each segment from a .bioMod file.
+    
+    Parameters
+    ----------
+    fichier_path : str
+        Path to the .bioMod file.
+    
+    Returns
+    ----------
+    masses : dict
+        Dictionary mapping each segment name (str) to its mass (float), 
+        as specified in the "mass" line in the segment definition.
+    """
+    masses = {}
+    with open(fichier_path, 'r') as fichier:
+        contenu = fichier.read()
+
+    segments = re.findall(r'segment\s+(\w+)(.*?)endsegment', contenu, re.DOTALL)
+
+    for nom_segment, bloc in segments:
+        match_masse = re.search(r'mass\s+([\d.]+)', bloc)
+        if match_masse:
+            masse = float(match_masse.group(1))
+            masses[nom_segment] = masse
+
+    return masses
+
+
+RAC = "/mnt/c/Users/emmam/biobuddy/examples/applied_examples/"
+
+all_data = []
+
+for n in range(1,577):
+    filename = RAC + f"athlete_{n}_deleva.bioMod"
+    masses_dict = extraire_masses(filename)
+    total_mass = 0
+    for segment, masse in masses_dict.items():
+        # print(f"{segment} : {masse} kg")
+        total_mass+= masse
+
+# print(total_mass)
+    masses_dict["total_mass"] = total_mass
+    row = pd.DataFrame([masses_dict], index=[n])
+    all_data.append(row)
+
+df_all = pd.concat(all_data)
+df_all.index.name = "athlete"
+
+# print(df_all)
+
+df_all.to_csv(RAC + "masses.csv")
+print("fichier csv ok")
